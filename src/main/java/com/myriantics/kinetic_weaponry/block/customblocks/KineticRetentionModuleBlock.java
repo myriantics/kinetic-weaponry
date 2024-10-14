fc17f12ef219223ba8c6039ddd0c4372aa1d511a
thatsmartguy21 <thatsmartguy21@gmail.com>
2024-10-13 23:18:16 -0700

    Added block visuals for Kinetic Retention Module

--- src/main/java/com/myriantics/kinetic_weaponry/block/customblocks/KineticRetentionModuleBlock.java
+++ src/main/java/com/myriantics/kinetic_weaponry/block/customblocks/KineticRetentionModuleBlock.java
@@ -5,7 +5,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.WallBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
@@ -17,7 +16,7 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
 public class KineticRetentionModuleBlock extends Block {
-    public static final IntegerProperty KINETIC_CHARGE = KineticWeaponryBlockStateProperties.KINETIC_CHARGE;
+    public static final IntegerProperty KINETIC_CHARGE = KineticWeaponryBlockStateProperties.KINETIC_RELOAD_CHARGES;
     public static final DirectionProperty FACING = BlockStateProperties.FACING;
     public static final BooleanProperty LIT = BlockStateProperties.LIT;
     public static final BooleanProperty POWERED = BlockStateProperties.POWERED;
@@ -28,13 +27,18 @@ public class KineticRetentionModuleBlock extends Block {
 
     @Override
     protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
-        VoxelShape baseShape = Block.box(0.0, 0.0, 0.0, 3.0, 3.0, 3.0);
-        return baseShape;
-    }
+        VoxelShape shape = null;
 
-    @Override
-    protected VoxelShape getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
-        return super.getCollisionShape(state, level, pos, context);
+        switch(state.getValue(FACING)) {
+            case UP -> shape = Block.box(5.0, 6.0, 5.0, 11.0, 16.0, 11.0);
+            case DOWN -> shape = Block.box(5.0, 0.0, 5.0, 11.0, 10.0, 11.0);
+            case NORTH -> shape = Block.box(5.0, 5.0, 0.0, 11.0, 11.0, 10.0);
+            case EAST -> shape = Block.box(6.0, 5.0, 5.0, 16.0, 11.0, 11.0);
+            case SOUTH -> shape = Block.box(5.0, 5.0, 6.0, 11.0, 11.0, 16.0);
+            case WEST -> shape = Block.box(0.0, 5.0, 5.0, 10.0, 11.0, 11.0);
+        }
+
+        return shape;
     }
 
     @Override
@@ -45,6 +49,6 @@ public class KineticRetentionModuleBlock extends Block {
     @Nullable
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
-        return super.getStateForPlacement(context);
+        return this.defaultBlockState().setValue(FACING, context.getClickedFace().getOpposite());
     }
 }
