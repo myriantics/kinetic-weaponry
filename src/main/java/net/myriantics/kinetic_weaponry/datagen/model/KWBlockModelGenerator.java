6fdd9213227641f904496bba85d992f93ea31256
myriantics <myriantics@proton.me>
2026-02-16 15:22:51 -0800

    Add model datagen for Kinetic Detonator

--- src/main/java/net/myriantics/kinetic_weaponry/datagen/model/KWBlockModelGenerator.java
+++ src/main/java/net/myriantics/kinetic_weaponry/datagen/model/KWBlockModelGenerator.java
@@ -29,11 +29,20 @@ public class KWBlockModelGenerator {
     }
 
     public void generateModels() {
+        generateFacing(KWBlocks.KINETIC_DETONATOR);
+
         generateKineticRetentionModule(KWBlocks.KINETIC_RETENTION_MODULE, StandardKineticRetentionModuleBlock.KINETIC_CHARGE, KWTexturedModels.STANDARD_KINETIC_RETENTION_MODULE);
         generateKineticRetentionModule(KWBlocks.CREATIVE_KINETIC_RETENTION_MODULE, null, KWTexturedModels.STANDARD_KINETIC_RETENTION_MODULE);
         generateKineticRetentionModule(KWBlocks.LESSER_KINETIC_RETENTION_MODULE, LesserKineticRetentionModuleBlock.KINETIC_CHARGE, KWTexturedModels.LESSER_KINETIC_RETENTION_MODULE);
         generateKineticRetentionModule(KWBlocks.CREATIVE_LESSER_KINETIC_RETENTION_MODULE, null, KWTexturedModels.LESSER_KINETIC_RETENTION_MODULE);
+
         generateTrialWeave(KWBlocks.TRIAL_WEAVE);
+    }
+
+    private void generateFacing(Block block) {
+        ResourceLocation resourceLocation = TexturedModel.CUBE_TOP_BOTTOM.create(block, this.generator.modelOutput);
+        this.generator.blockStateOutput.accept(MultiVariantGenerator.multiVariant(block, Variant.variant().with(VariantProperties.MODEL, resourceLocation)).with(createUpDefaultRotationStates()));
+        this.generator.delegateItemModel(block, resourceLocation);
     }
 
     private void generateTrialWeave(Block block) {
@@ -88,5 +97,15 @@ public class KWBlockModelGenerator {
                 .select(Direction.SOUTH, Variant.variant().with(VariantProperties.X_ROT, VariantProperties.Rotation.R90))
                 .select(Direction.EAST, Variant.variant().with(VariantProperties.X_ROT, VariantProperties.Rotation.R90).with(VariantProperties.Y_ROT, VariantProperties.Rotation.R270))
                 .select(Direction.WEST, Variant.variant().with(VariantProperties.X_ROT, VariantProperties.Rotation.R90).with(VariantProperties.Y_ROT, VariantProperties.Rotation.R90));
+    }
+
+    public static PropertyDispatch createUpDefaultRotationStates() {
+        return PropertyDispatch.property(BlockStateProperties.FACING)
+                .select(Direction.UP, Variant.variant())
+                .select(Direction.DOWN, Variant.variant().with(VariantProperties.X_ROT, VariantProperties.Rotation.R180))
+                .select(Direction.NORTH, Variant.variant().with(VariantProperties.X_ROT, VariantProperties.Rotation.R90))
+                .select(Direction.SOUTH, Variant.variant().with(VariantProperties.X_ROT, VariantProperties.Rotation.R270))
+                .select(Direction.EAST, Variant.variant().with(VariantProperties.X_ROT, VariantProperties.Rotation.R90).with(VariantProperties.Y_ROT, VariantProperties.Rotation.R90))
+                .select(Direction.WEST, Variant.variant().with(VariantProperties.X_ROT, VariantProperties.Rotation.R90).with(VariantProperties.Y_ROT, VariantProperties.Rotation.R270));
     }
 }
