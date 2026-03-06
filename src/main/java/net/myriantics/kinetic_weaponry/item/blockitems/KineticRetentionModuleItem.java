69bfbceb6d42e95ce41d157aeb83ec54e7ddf84d
myriantics <myriantics@proton.me>
2026-03-05 17:58:40 -0800

    Kinetic Retention Modules now have attribute modifiers according to their ArmorMaterial

--- src/main/java/net/myriantics/kinetic_weaponry/item/blockitems/KineticRetentionModuleItem.java
+++ src/main/java/net/myriantics/kinetic_weaponry/item/blockitems/KineticRetentionModuleItem.java
@@ -2,7 +2,14 @@ package net.myriantics.kinetic_weaponry.item.blockitems;
 
 import net.fabricmc.fabric.api.item.v1.EquipmentSlotProvider;
 import net.minecraft.core.Holder;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EquipmentSlotGroup;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.item.component.ItemAttributeModifiers;
+import net.myriantics.kinetic_weaponry.KWCommon;
 import net.myriantics.kinetic_weaponry.block.retention_module.AbstractKineticRetentionModuleBlock;
 import net.myriantics.kinetic_weaponry.mechanics.kinetic_charge.KineticItem;
 import net.minecraft.world.InteractionHand;
@@ -20,7 +27,7 @@ public class KineticRetentionModuleItem extends BlockItem implements Equipable, 
     private final Holder<ArmorMaterial> material;
 
     public KineticRetentionModuleItem(Block block, ArmorItem.Type type, Holder<ArmorMaterial> material, Properties properties) {
-        super(block, properties);
+        super(block, properties.component(DataComponents.ATTRIBUTE_MODIFIERS, createAttributeModifiers(material, type)));
         this.type = type;
         this.material = material;
         if (!(block instanceof AbstractKineticRetentionModuleBlock)) {
@@ -45,6 +52,26 @@ public class KineticRetentionModuleItem extends BlockItem implements Equipable, 
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
         return this.swapWithEquipmentSlot(this, level, player, usedHand);
+    }
+
+    public static ItemAttributeModifiers createAttributeModifiers(Holder<ArmorMaterial> material, ArmorItem.Type type) {
+        int defense = material.value().getDefense(type);
+        float toughness = material.value().toughness();
+        float knockbackResistance = material.value().knockbackResistance();
+
+        ItemAttributeModifiers.Builder builder = ItemAttributeModifiers.builder();
+        EquipmentSlotGroup equipmentSlotGroup = EquipmentSlotGroup.bySlot(type.getSlot());
+        ResourceLocation id = KWCommon.locate("armor." + type.getName());
+
+        builder.add(Attributes.ARMOR, new AttributeModifier(id, defense, AttributeModifier.Operation.ADD_VALUE), equipmentSlotGroup);
+        builder.add(Attributes.ARMOR_TOUGHNESS, new AttributeModifier(id, toughness, AttributeModifier.Operation.ADD_VALUE), equipmentSlotGroup);
+
+
+        if (knockbackResistance > 0.0F) {
+            builder.add(Attributes.KNOCKBACK_RESISTANCE, new AttributeModifier(id, knockbackResistance, AttributeModifier.Operation.ADD_VALUE), equipmentSlotGroup);
+        }
+
+        return builder.build();
     }
 
     /* @Override
