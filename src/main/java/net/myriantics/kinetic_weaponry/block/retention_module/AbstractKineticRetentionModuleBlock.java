396b533e3de2a1c60b1cdc364965a90e09c8d25d
myriantics <myriantics@proton.me>
2026-03-17 22:58:19 -0700

    Kinetic Retention Modules now update comparators when broken.

--- src/main/java/net/myriantics/kinetic_weaponry/block/retention_module/AbstractKineticRetentionModuleBlock.java
+++ src/main/java/net/myriantics/kinetic_weaponry/block/retention_module/AbstractKineticRetentionModuleBlock.java
@@ -114,6 +114,12 @@ public abstract class AbstractKineticRetentionModuleBlock extends BaseEntityBloc
     }
 
     @Override
+    protected void onRemove(BlockState state, Level level, BlockPos pos, BlockState newState, boolean movedByPiston) {
+        super.onRemove(state, level, pos, newState, movedByPiston);
+        level.updateNeighbourForOutputSignal(pos, this);
+    }
+
+    @Override
     protected boolean hasAnalogOutputSignal(BlockState state) {
         return true;
     }
